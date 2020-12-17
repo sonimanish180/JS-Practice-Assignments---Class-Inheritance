a6a48a8d9693a7beb01c018190dd18d0575a1959
sonimanish180 <68246299+sonimanish180@users.noreply.github.com>
2020-12-17 20:43:19 +0530

    changes4

--- src/components/Circle.js
+++ src/components/Circle.js
@@ -4,6 +4,10 @@ const Shape = require("./Shape.js");
 
 // declare class
 class Circle extends Shape {
+  constructor() {
+    super();
+    this.color = "blue";
+  }
   calculateArea = function () {
     console.log("Area");
   };
--- src/components/Shape.js
+++ src/components/Shape.js
@@ -2,13 +2,13 @@
 class Shape {
   constructor(color) {
     this.color = color;
-    this.drawShape = function () {
-      console.log("draw");
-    };
-    this.calculateArea = function () {
-      console.log("Area");
-    };
   }
+  drawShape = function () {
+    console.log("draw");
+  };
+  calculateArea = function () {
+    console.log("Area");
+  };
 }
 
 // export class using module.exports
