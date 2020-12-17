9db951f5cac354e1d3cf43a1ecb1c72b969a72e5
sonimanish180 <68246299+sonimanish180@users.noreply.github.com>
2020-12-17 13:37:19 +0530

    changes1

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,12 @@
 // import using require
+import { Shape } from "./shape";
 
 // declare class
-
+class Circle extends Shape {
+  calculateArea = function () {
+    console.log("Area");
+  };
+}
 
 // export class using module.exports
+module.exports(Circle);
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,15 @@
 // declare class
+class Shape {
+  constructor(color) {
+    this.color = color;
+    this.drawShape = function () {
+      console.log("draw");
+    };
+    this.calculateArea = function () {
+      console.log("Area");
+    };
+  }
+}
 
 // export class using module.exports
+module.exports(Shape);
