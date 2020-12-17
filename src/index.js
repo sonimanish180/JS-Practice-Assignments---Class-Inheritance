86391a077bc02105f107950ebad617f40865008d
sonimanish180 <68246299+sonimanish180@users.noreply.github.com>
2020-12-17 17:41:12 +0530

    Changes3

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
+// import App from "./components/App";
+const Shape = require("./components/Shape.js");
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Shape />, document.getElementById("root"));
